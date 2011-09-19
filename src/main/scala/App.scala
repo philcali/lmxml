e9d4c68c614339d0f61959742f3beeec97ee67ec
Philip Cali <philip.cali@gmail.com>
2011-09-19 11:38:04 -0500

    Majority of markup handling is done.

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -23,7 +23,10 @@ object LmxmlApp {
 }
 
 trait LmxmlParsers extends RegexParsers {
+  val increment: Int
+
   type NodeLike = (String, Map[String, String])
+  type Nodes = List[ParsedNode]
 
   // Meaningful whitespace
   override val whiteSpace = """\s+?""".r
@@ -31,6 +34,12 @@ trait LmxmlParsers extends RegexParsers {
   lazy val allwp = """\s*""".r
 
   lazy val everything = """(?!\s*[```])[^\n]+\n""".r
+
+  lazy val template = "[" ~ ident ~ "]"
+
+  lazy val templateDef = template ~ ":" ~ lmxml(increment) ^^ {
+    case ("[" ~ t ~ "]") ~ ":" ~ nodes => LinkDefinition(t, nodes)
+  }
 
   lazy val end = """(\r?\n)*"""
 
@@ -51,7 +60,7 @@ trait LmxmlParsers extends RegexParsers {
 
   lazy val textNode = (stringLit | alternateWrapper)
 
-  lazy val allNodes = (node | textNode)
+  lazy val allNodes = (node | textNode | templateDef | template)
 
   lazy val idAttr = "#" ~> ident ^^ {
     id => List(("id", id))
@@ -75,14 +84,18 @@ trait LmxmlParsers extends RegexParsers {
 
   def spaces(n: Int) = """\s{%d}""".format(n).r
 
-  def descending(d: Int = 0, incr: Int = 2): Parser[Any] = 
-   opt(spaces(d)) ~> allNodes ~ rep(descending(d + incr) | allNodes)
+  def descending(d: Int = 0): Parser[Any] = 
+   opt(spaces(d)) ~> allNodes ~ rep(descending(d + increment) | allNodes)
 
-  private def descend(in: List[Any]): List[ParsedNode] = in match {
+  private def descend(in: List[Any]): Nodes = in match {
     case (h ~ ns) :: rest =>
       val ls = ns.asInstanceOf[List[_]]
 
       val n = h match {
+        // Pass Through
+        case l: LinkDefinition => l
+        case "[" ~ l ~ "]" =>
+          TemplateLink(l.toString, descend(ls))
         case s: String => 
           TextNode(s, descend(ls))
         case _ =>
@@ -94,12 +107,37 @@ trait LmxmlParsers extends RegexParsers {
     case Nil => Nil
   }
 
-  lazy val lmxml = rep(descending(0)) ^^ {
-    case all => descend(all)
+  def lmxml(startingDepth: Int = 0) = rep(descending(startingDepth)) ^^ {
+    case all => 
+      val nodes = descend(all)
+      val (linkDefs, other) = nodes.partition(_.isInstanceOf[LinkDefinition])
+      linkDefs.foldLeft(other) { (rebuilt, linkDef) =>
+        rebuild(rebuilt, linkDef.asInstanceOf[LinkDefinition])
+      }
+  }
+
+  private def rebuild(n: Nodes, link: LinkDefinition): Nodes = n match {
+    case h :: rest => 
+      def rebuildNode(y: ParsedNode): ParsedNode = y.children match {
+        case (t: TemplateLink) :: cs if (t.name == link.name) =>
+          copyNode(y, link.children ++ rebuild(t.children, link) ++ rebuild(cs, link))
+        case t :: cs => copyNode(y, rebuildNode(t) :: rebuild(cs, link))
+        case _ => copyNode(y, rebuild(y.children, link))
+      }
+
+      rebuildNode(h) :: rebuild(rest, link)
+    case Nil => Nil
+  }
+
+  private def copyNode(n: ParsedNode, nodes: Nodes) = n match {
+    case l: LmxmlNode => l.copy(children = nodes)
+    case t: TextNode => t.copy(children = nodes)
+    case _ => n
   }
 }
 
 trait ParsedNode {
+  val name: String
   val children: List[ParsedNode]
 }
 
@@ -112,23 +150,36 @@ case class LmxmlNode(
 case class TextNode(
   contents: String, 
   children: List[ParsedNode] = Nil
+) extends ParsedNode {
+  val name = "text"
+}
+
+case class TemplateLink(
+  name: String,
+  children: List[ParsedNode] = Nil
+) extends ParsedNode
+
+case class LinkDefinition(
+  name: String,
+  children: List[ParsedNode] = Nil
 ) extends ParsedNode
 
 object Lmxml extends LmxmlParsers {
+  val increment = 2
 
   def pretty(nodes: List[ParsedNode], depth: Int = 0) {
     val tab = (0 to depth).map(_ => " ").mkString("")
 
     for(node <- nodes) {
-      println("%s%s".format(tab, node)) 
+      println("%s%s".format(tab, node.name)) 
       pretty(node.children, depth + 2)
     }
   }
 
   def apply(contents: String) = {
 
-    phrase(lmxml)(new CharSequenceReader(contents)) match {
-      case Success(result, _) => println(result)
+    phrase(lmxml(0))(new CharSequenceReader(contents)) match {
+      case Success(result, _) => pretty(result)
       case n @ _ => println(n)
     }
   }
