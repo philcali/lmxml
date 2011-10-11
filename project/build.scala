19b4b484b75dbc5bfbe813447e73ac42885fac26
Philip Cali <philip.cali@gmail.com>
2011-10-11 18:43:52 -0500

    Bumping sbt interface

--- project/build.scala
+++ project/build.scala
@@ -26,7 +26,13 @@ object LmxmlBuild extends Build {
     settings = generalSettings ++ Seq (
       scalaVersion := "2.9.1",
       crossScalaVersions ++=
-        Seq("2.8.0", "2.8.1", "2.9.0", "2.9.0-1", "2.9.1")
+        Seq("2.8.0", "2.8.1", "2.9.0", "2.9.0-1", "2.9.1"),
+      libraryDependencies <+= (scalaVersion) {
+        case v if v.startsWith("2.8") =>
+          "org.scalatest" %% "scalatest" % "1.3"
+        case _ =>
+          "org.scalatest" %% "scalatest" % "1.6.1"
+      }
     )
   )
 }
