34a6f95ac8469863a797996a05a8a935a6e821f4
Philip Cali <philip.cali@gmail.com>
2012-01-19 00:48:23 +0000

    Initate plan for caching

---
+++ cache/src/main/scala/cache.scala
@@ -0,0 +1,19 @@
+package lmxml
+package cache
+
+/*
+source changed?
+     |
+     |
+  -------
+  |     |
+ No    Yes---------------
+  |                     |
+  |                     |
+  |                     -----parse, serialize, md5, save, convert
+Load from
+cache, deserialize,
+and convert
+*/
+
+
