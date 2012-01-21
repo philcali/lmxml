8e17af9447a961350b95f2c5a796076896df16cc
Philip Cali <philip.cali@gmail.com>
2012-01-21 13:35:30 -0600

    Fleshing out cache API

--- cache/src/main/scala/cache.scala
+++ cache/src/main/scala/cache.scala
@@ -24,21 +24,80 @@ import java.security.{
   DigestInputStream
 }
 
-object HashLogic {
-  def hash(file: File) = {
-    val ins = new FileInputStream(file)
+object BasicFileStorage extends FileHashes {
+}
 
+trait FileHashes extends HashLogic {
+  val location: File
+
+  require(location.isDirectory)
+
+  def retrieve(hash: String) = {
+    val file = new File(location, hash)
+
+    if (!file.exists || file.isFile)
+      None
+    else
+      file.listFiles.find(_.isFile).map(_.getName)
+  }
+
+  def store(file: File) {
+    // Serialize
+    
+  }
+
+  def data(file: File) {
+  }
+}
+
+trait HashLogic extends FileLoading {
+  def retrieve(hash: String): Option[String]
+
+  def clear: Boolean
+
+  override def fromFile[A](file: File)(implicit converter: Seq[ParsedNode] => A) = {
+    if (changed(file)) {
+    }
+
+    import scala.io.Source.{fromFile => open}
+
+    val text = open(file).getLines.mkString("\n")
+
+    convert(text)(converter)
+  }
+
+  def changed(file: File) = { 
+    retrieve(hashFilename(file)).map(_ != (hashContents(file)).getOrElse(true)
+  }
+
+  def hashContents(file: File) = {
+    hash { md =>
+      val ins = new FileInputStream(file)
+
+      val di = new DigestInputStream(ins, md)
+
+      try {
+        while (di.read() > 0) {
+          di.read()
+        }
+      } finally {
+        di.close()
+      }
+    }
+  }
+
+  def hashFilename(file: File) = {
+    hash { md =>
+      val byteNames = file.getAbsolutePath.getBytes
+
+      md.update(byteNames, 0, byteNames.length
+    }
+  }
+
+  def hash(fun: MessageDigest => Unit) = {
     val md = MessageDigest.getInstance("MD5")
 
-    val di = new DigestInputStream(ins, md)
-
-    try {
-      while (di.read() > 0) {
-        di.read()
-      }
-    } finally {
-      di.close()
-    }
+    fun(md)
 
     val digest = md.digest()
 
